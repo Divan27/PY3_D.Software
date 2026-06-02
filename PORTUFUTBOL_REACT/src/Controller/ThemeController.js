09b7f4eff0a2c9e79acaa5d7174de828be35b44d
Divan27 <49537341+Divan27@users.noreply.github.com>
2026-06-02 10:26:40 -0600

    Carga del controlador del tema
    Tema oscuro y claro ajustado

---
+++ PORTUFUTBOL_REACT/src/Controller/ThemeController.js
@@ -0,0 +1,32 @@
+import ThemeManager from "../Model/ThemeManager";
+
+class ThemeController {
+  static instance = null;
+
+  constructor() {
+    if (ThemeController.instance) {
+      return ThemeController.instance;
+    }
+
+    this.themeManager = ThemeManager.getInstance();
+    ThemeController.instance = this;
+  }
+
+  static getInstance() {
+    if (!ThemeController.instance) {
+      ThemeController.instance = new ThemeController();
+    }
+
+    return ThemeController.instance;
+  }
+
+  getTheme() {
+    return this.themeManager.getTheme();
+  }
+
+  toggleTheme() {
+    this.themeManager.toggleTheme();
+  }
+}
+
+export default ThemeController;
