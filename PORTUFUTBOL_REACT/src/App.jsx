38ba8deff566d0306cf2ee64aec56c6bdda85aaf
DaryllMR <103915598+DaryllMR@users.noreply.github.com>
2026-06-10 02:02:39 -0600

    Update App.jsx

--- PORTUFUTBOL_REACT/src/App.jsx
+++ PORTUFUTBOL_REACT/src/App.jsx
@@ -3,42 +3,30 @@ import InicioView from "./View/InicioView";
 import ThemeController from "./Controller/ThemeController";
 
 function App() {
-
     const controller = ThemeController.getInstance();
 
-    const [theme, setTheme] = useState(
-        controller.getTheme()
-    );
+    const [theme, setTheme] = useState(controller.getTheme());
+    const [colorBlindMode, setColorBlindMode] = useState("normal");
 
     useEffect(() => {
-
-        document.body.className = theme;
-
-    }, [theme]);
+        document.body.className = `${theme} ${colorBlindMode}`;
+    }, [theme, colorBlindMode]);
 
     const toggleTheme = () => {
-
         controller.toggleTheme();
-
-        setTheme(
-            controller.getTheme()
-        );
-
+        setTheme(controller.getTheme());
     };
 
     return (
-
-        <div className={`app ${theme}`}>
-
+        <div className={`app ${theme} ${colorBlindMode}`}>
             <InicioView
                 theme={theme}
                 toggleTheme={toggleTheme}
+                colorBlindMode={colorBlindMode}
+                setColorBlindMode={setColorBlindMode}
             />
-
         </div>
-
     );
-
 }
 
 export default App;
